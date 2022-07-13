f7b232796fb93c9ab044c48893c5d16c36e2c94e
Art <WildandArt@users.noreply.github.com>
2022-07-13 06:51:12 +0300

    Initial Commit

---
+++ AppStore-UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStore-UI
+//
+//  Created by Artemy Ozerski on 13/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
